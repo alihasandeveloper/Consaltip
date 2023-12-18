4a7ffd38007e05a5364e4cf828d9afb7f10c3a65
alihasandeveloper <alihasandeveloper@gmail.com>
2023-12-18 16:50:33 +0600

    slider adder

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ var loader = document.getElementById("preloader");
 window.addEventListener("load", function(){
     loader.style.display = "none";
 })
+
+
+
+$('.service-card-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
